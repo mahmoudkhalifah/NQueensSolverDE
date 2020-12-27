ca19e661bbdadd613a00ce487a7166767b6e9f0d
Mahmoud khalifa <57409035+mahmoudkhalifah@users.noreply.github.com>
2020-12-28 00:17:01 +0200

    population initialization edited

--- PopulationGenerator.py
+++ PopulationGenerator.py
@@ -4,8 +4,14 @@ import random
 class PopulationGenerator:
 
     def __init__(self, dim, NP):
-        self.NPvectors = [[random.randint(0, dim - 1) for i in range(dim)] for j in range(NP)]
-
+        # self.NPvectors = [[random.randint(0, dim - 1) for i in range(dim)] for j in range(NP)]
+        self.NPvectors = [[-1 for i in range(dim)] for j in range(NP)]
+        for i in range(NP):
+            for j in range(dim):
+                r = random.randint(0, dim - 1)
+                while r in self.NPvectors[i]:
+                    r = random.randint(0, dim - 1)
+                self.NPvectors[i][j] = r
 
     def printPopulation(self):
         print(self.NPvectors)
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from DifferentialEvolution import DifferentialEvolution
-from PopulationGenerator import PopulationGenerator
 
-# numOfIterations = 15000
-# CR = 0.25
-# F = 1
-# timeLimit = 10*60
-# N = int(input("Enter number of queens\n"))
-# NP = N * 5
-# print("CR =", CR, "F =", F, "NP =", NP)
-pg = PopulationGenerator(4,40)
-pg.printPopulation()
-# solver = DifferentialEvolution(numOfIterations, CR, F, N, NP, timeLimit)
-# solver.solve()
+numOfIterations = 15000
+CR = 0.25
+F = 1
+timeLimit = 3*60
+N = int(input("Enter number of queens\n"))
+NP = N * 5
+print("CR =", CR, "F =", F, "NP =", NP)
+solver = DifferentialEvolution(numOfIterations, CR, F, N, NP, timeLimit)
+solver.solve()
